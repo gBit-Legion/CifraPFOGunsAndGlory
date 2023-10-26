f39d03592180e737af6e5abb0fa4b68afd863697
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-10-26 22:05:34 +0300

    main_update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import shutil
+
 from fastapi import FastAPI, Body, UploadFile, File, Form, Request
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
-from io import BytesIO
-from typing import Annotated
+
 
 app = FastAPI()
 
@@ -24,9 +25,7 @@ async def root():
 @app.post("/documents")
 async def drag_and_drop(files: list[UploadFile]):
     for file in files:
-        contents = await file.read()
-        file.close()
-        buffer = BytesIO(contents)
-        with open(file.filename, "wb") as f:
-            f.write(buffer.getbuffer())
-        print(file.file)
+        file_location = f"files/{file.filename}"
+        with open(file_location, "wb+") as file_object:
+            file_object.write(file.file.read())
+            print(file_object)
