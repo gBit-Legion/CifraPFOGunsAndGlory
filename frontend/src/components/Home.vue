66d8eeca1e6f5d77eec1a1213f5bd26c9b4d37ba
johnseeena <82346607+johnseeena@users.noreply.github.com>
2023-10-26 21:55:12 +0300

    front add

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="logo">
+        <img src="../image/Logo.png" alt="Логотип РЖД">
+    </div>
+    <Drag />
+</template>
+
+<style>
+@font-face {
+    font-family: "TTFirsNeue-Regular";
+    src: url("../fonts/TTFirsNeue-Regular.ttf");
+}
+
+@font-face {
+    font-family: "TTFirsNeue-bold";
+    src: url("../fonts/TTFirsNeue-Bold.ttf");
+}
+
+* {
+    font-family: TTFirsNeue-Regular;
+}
+
+.logo {
+    width: 200px;
+    margin: 0 auto;
+}
+</style>
+
+<script>
+import Drag from './Drag.vue';
+
+export default {
+    components: {
+        Drag
+    },
+
+    methods: {
+
+    }
+}
+</script>
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, UploadFile, File, Form, Request
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
+from io import BytesIO
+from typing import Annotated
 
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,4 +18,15 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return FileResponse("frontend/public/index.html")
+
+
+@app.post("/documents")
+async def drag_and_drop(files: list[UploadFile]):
+    for file in files:
+        contents = await file.read()
+        file.close()
+        buffer = BytesIO(contents)
+        with open(file.filename, "wb") as f:
+            f.write(buffer.getbuffer())
+        print(file.file)
